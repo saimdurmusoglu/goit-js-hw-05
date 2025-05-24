8b82c95dce3a3bd63a184cd68cace0bd18bfce01
Saim Durmuşoğlu <saim.durmusoglu@gmail.com>
2025-05-24 22:46:36 +0300

    24.05.2025 22:46 - düzeltme yapıldı

--- js/task-4.js
+++ js/task-4.js
@@ -1,8 +1,9 @@
-const getTotalBalanceByGender = (users, gender) => {
-  return users.reduce((total, user) => {
-    return user.gender === gender ? total + user.balance : total;
-  }, 0);
-};
+function getTotalBalanceByGender(users, gender) {
+  return users
+    .filter(user => user.gender === gender)
+    .map(user => user.balance)
+    .reduce((total, balance) => total + balance, 0);
+}
 
 const allUsers2 = [
   {
